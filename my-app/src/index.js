5eb9aa406d1d394894492ed5666aaa7bad3caa21
Gaëtan <eag53@hotmail.fr>
2024-11-18 12:54:35 +0100

    Correction fichier index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Apartments from './pages/Apartments'
+import About from './pages/About'
+import Error from './pages/Error'
+import Header from './components/Header'
+import './styles/app.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ 
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/apartments" element={<Apartments />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Router>
+        </React.StrictMode>,
   document.getElementById('root')
-);
+)
