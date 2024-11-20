a10c569d8c37365f61b9c40c68b6cd45aa59bacf
Gaëtan <eag53@hotmail.fr>
2024-11-20 23:21:53 +0100

    Ajout import Footer et CSS

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,20 +6,23 @@ import Apartments from './pages/Apartments'
 import About from './pages/About'
 import Error from './pages/Error'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import './styles/index.css'
 
  
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/apartments" element={<Apartments />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </Router>
-        </React.StrictMode>,
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apartments" element={<Apartments />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 );
