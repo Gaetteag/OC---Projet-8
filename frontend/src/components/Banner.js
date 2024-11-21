b38ac7ce64c8c14fe925bf543f878e5ab5e73c10
Gaëtan <eag53@hotmail.fr>
2024-11-21 12:33:50 +0100

    Ajout balise Fig dans Banner

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -3,9 +3,11 @@ import HomeBannerImage from '../assets/HomeBannerImage.svg'
 
 function Banner() {
     return (
-        <section className='homeBanner'> 
-            <img src={HomeBannerImage} alt="Bannière de la page d'accueil"></img>
-            <h1>Chez vous, partout et ailleurs</h1>
+        <section>
+            <fig className='homeBanner'>
+                <img src={HomeBannerImage} alt="Bannière de la page d'accueil"></img>
+                <h1>Chez vous, partout et ailleurs</h1>
+            </fig>
         </section>
     )
 }
