e5b93a325f899e40e0d2034fbc4f4914811bd025
Gaëtan <eag53@hotmail.fr>
2024-11-21 12:30:09 +0100

    Ajout balise main dans Home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,10 +4,10 @@ import Card from '../components/Card';
 
 function Home() {
     return (
-        <>
+        <main>
             <Banner />
             <Card />
-        </>
+        </main>
     )
 }
 
