750079504bf2f1a40841b5ac3baf377c39d12294
Gaëtan <eag53@hotmail.fr>
2024-11-24 15:43:53 +0100

    Transfert balise section dans Home.js

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,14 +1,12 @@
-import '../styles/card.css'
+import '../styles/card.css';
 
-function Card({ApartmentCover, ApartmentTitle}) {
+function Card({ ApartmentCover, ApartmentTitle }) {
     return (
-        <section className='cards-container'> 
-            <figure className='card'>
-                <img src={ApartmentCover} alt="Appartement" className='cardImage'></img>
-                <figcaption className='cardTitle'>{ApartmentTitle}</figcaption>
-            </figure>
-        </section>
-    )
+        <figure className="card">
+            <img src={ApartmentCover} alt={ApartmentTitle} className="cardImage" />
+            <figcaption className="cardTitle">{ApartmentTitle}</figcaption>
+        </figure>
+    );
 }
 
-export default Card
+export default Card;
