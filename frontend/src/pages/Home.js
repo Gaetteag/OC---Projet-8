053eb4bf34ce9b11cf379f8b3c93d126cd7ed7cf
Gaëtan <eag53@hotmail.fr>
2024-11-24 15:45:41 +0100

    Ajout section + affichage appartements via API

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,42 @@
-import '../styles/home.css'
-import Banner from '../components/Banner'
+import { useState, useEffect } from 'react';
+import '../styles/home.css';
+import Banner from '../components/Banner';
 import Card from '../components/Card';
 import HomeBannerImage from '../assets/HomeBannerImage.svg';
 
 function Home() {
+    const [properties, updateProperties] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/properties')
+            .then((response) => response.json())
+            .then((data) => {
+                updateProperties(data)
+            })
+            .catch((error) => console.error('Erreur lors de la récupération des données :', error));
+    }, []);
+
     return (
         <main>
-            <Banner 
+            <Banner
                 bannerImgSrc={HomeBannerImage}
                 bannerTitle="Chez vous, partout et ailleurs"
             />
-            <Card />
+            <section className="cards-container">
+                {properties.length > 0 ? (
+                    properties.map((property) => (
+                        <Card
+                            key={property.id}
+                            ApartmentCover={property.cover}
+                            ApartmentTitle={property.title}
+                        />
+                    ))
+                ) : (
+                    <p>Aucun appartement disponible</p>
+                )}
+            </section>
         </main>
-    )
+    );
 }
 
-export default Home
+export default Home;
