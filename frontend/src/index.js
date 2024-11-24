317759cc048676c4462476928fe81a40c9c89da3
Gaëtan <eag53@hotmail.fr>
2024-11-24 14:39:12 +0100

    Correction warning index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Apartments from './pages/Apartments'
@@ -9,7 +9,6 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import './styles/index.css'
 
- 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -23,6 +22,5 @@ root.render(
         </Routes>
         <Footer />
       </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
