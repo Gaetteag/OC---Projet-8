0a0441bf16077a7720872423f87d3400b4394e26
Gaëtan <eag53@hotmail.fr>
2024-11-26 14:50:28 +0100

    Titre collapses page About créé dynamiquement

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,16 +1,25 @@
 import '../styles/about.css'
 import Banner from '../components/Banner'
+import Collapse from '../components/Collapse'
 import AboutBannerImage from '../assets/AboutBannerImage.svg';
+import collapseContent from '../collapseContent'
 
 function About() {
     return (
         <main>
-            <p>Page à-propos</p>
             <Banner 
                 bannerImgSrc={AboutBannerImage}
-            />
+            />            
+            <section className='collapse-container'>
+                {collapseContent.map((item, index) => (
+                    <Collapse 
+                        key={index} 
+                        title={item.title} 
+                    />
+                ))}
+            </section>
         </main>
     )
 }
 
-export default About;
+export default About
