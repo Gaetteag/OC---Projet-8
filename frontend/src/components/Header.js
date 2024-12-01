89d8f3632edc104533d575e39d8eccb2f1925733
Gaëtan <eag53@hotmail.fr>
2024-12-02 00:05:52 +0100

    Lien header souligné selon la page active

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,25 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import '../styles/header.css'
 import LogoHeaderDesktop from '../assets/LogoHeaderDesktop.svg'
 
 function Header() {
+    const location = useLocation();
+
     return (
         <header> 
             <nav className='navbar'>
                 <Link to="/"><img src={LogoHeaderDesktop} alt="Logo Kasa"></img></Link>
                 <ul className='links-navbar'>
-                    <li><Link to="/" className='activePage'>Accueil</Link></li>
-                    <li><Link to="/about">A Propos</Link></li>
+                    <li>
+                        <Link to="/" className={location.pathname === '/' ? 'activePage' : ''}>
+                            Accueil
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className={location.pathname === '/about' ? 'activePage' : ''}>
+                            A Propos
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </header>
