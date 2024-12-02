a989e981bedc7ed920df2e1ed4a54860209161d8
Gaëtan <eag53@hotmail.fr>
2024-12-02 11:50:58 +0100

    Modification page apartments
    Ajout lien vers page d'erreur en cas de mauvais id
    Ajout logique d'affichage des données

--- frontend/src/pages/Apartments.js
+++ frontend/src/pages/Apartments.js
@@ -1,7 +1,32 @@
-import '../styles/apartments.css'
+import { useParams, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function Apartments() {
-    return <div>Page de location des appartements</div>;
+    const { id } = useParams();
+    const [apartment, setApartment] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch('http://localhost:8080/api/properties')
+            .then((response) => response.json())
+            .then((apartments) => {
+                const findApartment = apartments.find((apartment) => apartment.id === id);
+                findApartment ? setApartment(findApartment) : navigate('/erreur');
+            })
+            .catch((error) => {
+                console.error('Erreur lors de la récupération des données :', error);
+                navigate('/erreur');
+            });
+    }, [id, navigate]);
+
+    return apartment ? (
+        <div className='apartmentPage'>
+            <h1>{apartment.title}</h1>
+            <img src={apartment.cover} alt={apartment.title} />
+        </div>
+    ) : (
+        <p>L4appartement n'a pas été trouvé.</p>
+    );
 }
 
 export default Apartments;
