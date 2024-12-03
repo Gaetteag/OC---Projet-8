571dc606fdd94cb0a2a82dbff0f6c48e172cb163
Gaëtan <eag53@hotmail.fr>
2024-12-03 21:30:28 +0100

    Création composant Carrousel

---
+++ frontend/src/components/Carrousel.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import LeftArrow from '../assets/LeftArrow.svg';
+import RightArrow from '../assets/RightArrow.svg';
+import '../styles/carrousel.css';
+
+function Carrousel({ pictures, title }) {
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const goToNextImage = () => {
+        if (pictures && pictures.length > 0) {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % pictures.length);
+        }
+    };
+
+    const goToPreviousImage = () => {
+        if (pictures && pictures.length > 0) {
+            setCurrentImageIndex((prevIndex) =>
+                prevIndex === 0 ? pictures.length - 1 : prevIndex - 1
+            );
+        }
+    };
+
+    return (
+        <div className="apartmentCarrousel">
+            <img src={pictures[currentImageIndex]} alt={title} className="apartmentCarrouselImg" />
+            {pictures.length > 1 && (
+                <div>
+                    <button className="arrow arrow_left" onClick={goToPreviousImage}>
+                        <img src={LeftArrow} alt="flèche pour défiler vers la gauche" />
+                    </button>
+                    <button className="arrow arrow_right" onClick={goToNextImage}>
+                        <img src={RightArrow} alt="flèche pour défiler vers la droite" />
+                    </button>
+                </div>
+            )}
+            {pictures.length > 1 && (
+                <div className="imageIndex">
+                    <span>{currentImageIndex + 1}/{pictures.length}</span>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Carrousel;
