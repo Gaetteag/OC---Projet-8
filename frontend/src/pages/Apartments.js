b2aada19fcd43829296cc31c4005a73a5398cbee
Gaëtan <eag53@hotmail.fr>
2024-12-03 21:29:57 +0100

    Transfert Carrousel vers composant dédié

--- frontend/src/pages/Apartments.js
+++ frontend/src/pages/Apartments.js
@@ -1,14 +1,12 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import Carrousel from '../components/Carrousel';
 import Collapse from '../components/Collapse';
-import LeftArrow from '../assets/LeftArrow.svg';
-import RightArrow from '../assets/RightArrow.svg';
 import '../styles/apartments.css'
 
 function Apartments() {
     const { id } = useParams();
     const [apartment, setApartment] = useState(null);
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -24,40 +22,11 @@ function Apartments() {
             });
     }, [id, navigate]);
 
-    const goToNextImage = () => {
-        if (apartment.pictures && apartment.pictures.length > 0) {
-            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % apartment.pictures.length);
-        }
-    };
-
-    const goToPreviousImage = () => {
-        if (apartment.pictures && apartment.pictures.length > 0) {
-            setCurrentImageIndex((prevIndex) =>
-                prevIndex === 0 ? apartment.pictures.length - 1 : prevIndex - 1
-            );
-        }
-    };
-
     return apartment ? (
         <div className='apartmentPage'>
-            <div className='apartmentCarrousel'>
-                <img src={apartment.pictures[currentImageIndex]} alt={apartment.title} className='apartmentCarrouselImg' />
-                {apartment.pictures.length > 1 && (
-                    <div>
-                        <button className="arrow arrow_left" onClick={goToPreviousImage}>
-                            <img src={LeftArrow} alt="flèche pour défiler vers la gauche" />
-                        </button>
-                        <button className="arrow arrow_right" onClick={goToNextImage}>
-                            <img src={RightArrow} alt="flèche pour défiler vers la droite" />
-                        </button>
-                    </div>
-                )}
-                {apartment.pictures.length > 1 && (
-                    <div className="imageIndex">
-                        <p>{currentImageIndex + 1}/{apartment.pictures.length}</p>
-                    </div>
-                )}
-            </div>
+            <Carrousel 
+                pictures={apartment.pictures}
+            />
             <h1>{apartment.title}</h1>
             <p className='apartmentLocation'>{apartment.location}</p>
             <div className='apartmentsTags'>
@@ -65,12 +34,21 @@ function Apartments() {
                     <span key={index} className="apartmentTag">{tag}</span>
                 ))}
             </div>
-            <p className='hostName'>
-                <span>{apartment.host.name.split(' ')[0]}</span>{/* Prénom */}
-                <span>{apartment.host.name.split(' ')[1]}</span>{/* Nom */}
-            </p>
-            <img src={apartment.host.picture} alt={apartment.host.name} className='hostPicture'/>
-            <p >{apartment.rating}</p>
+            <div className='hostInformations'>
+                <p className='hostName'>
+                    <span>{apartment.host.name.split(' ')[0]}</span>{/* Prénom */}
+                    <span>{apartment.host.name.split(' ')[1]}</span>{/* Nom */}
+                </p>
+                <img src={apartment.host.picture} alt={apartment.host.name} className='hostPicture'/>
+            </div>
+            <div className='apartmentRating'>
+                <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                <i className="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                <i className="fa-xs fa-solid fa-star neutral-star" aria-hidden="true"></i>
+                <span className="sr-only">Note de 4 sur 5</span>
+            </div>
             <Collapse>
                 <p>test collapse</p>
             </Collapse>
